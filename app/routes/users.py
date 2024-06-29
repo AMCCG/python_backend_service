2dc1a26397b465ac3dd23202dc75718cfedf5a9a
apisit.a <amornchanchaigul@gmail.com>
2024-06-29 22:41:02 +0700

    update router

--- app/routes/users.py
+++ app/routes/users.py
@@ -1,14 +1,16 @@
+from app.domain.user.user_model import UserModel
 from app.util.util_constant import Constant
 from fastapi import APIRouter
 
 router = APIRouter()
 @router.get(Constant.ROOT_PATH+"/users", tags=["users"])
 def fetch_user():
+   
     return [
-        {"id": 1, "username": "John"},
-        {"id": 2, "username": "Jame"}
+        UserModel(id=1,username="John Python"),
+
     ]
 
 @router.get(Constant.ROOT_PATH+"/users/{item_id}", tags=["users"])
 def fetch_user_by_id(item_id: int):
-    return {"id": item_id, "username": "John"}
+    return UserModel(id=item_id,username="John Python")
